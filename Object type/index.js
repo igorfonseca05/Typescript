ff077facda866918df4c30f2e080aa88c3ecac7c
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-04-29 11:08:21 -0300

    Object types section

--- Object type/index.js
+++ Object type/index.js
@@ -34,10 +34,70 @@
     // showUser(user)
 })();
 // 76. Propriedades readonly
+/**A propriedade read-only impede o valor do objeto de ser alterado */
+(() => {
+    function showCarsInfos(car) {
+        console.log(`${car.nome} is a car made by ${car.brand} and has ${car.wheel} wheels`);
+    }
+    const car = {
+        nome: 'Palio',
+        brand: 'Fiat',
+        wheel: 4
+    };
+    showCarsInfos(car);
+})();
 // 77. Index Signature
+(() => {
+    /** Permite criarmos objetos de maneira livre, podem seguindo a sintaxe de que
+     * a chave deve ser uma string e o valor tbm
+     */
+    const user = {
+        name: 'Caio',
+        age: '30'
+    };
+})();
 // 78. Herança de interfaces
+/** A idéia por trás da herança é que usando o extends não precisaremos reescrever as propriedades de uma
+ * interface que queremos usar em outra interface
+ */
+(() => {
+    const sponge = {
+        name: 'sponge',
+        price: 2.36,
+        isAvaliable: true
+    };
+})();
 // 79. Intersection types
+/**Aqui usamos o intersection types para unir duas interfaces em um unico type*/
+(() => {
+    const user = {
+        name: 'Igor',
+        cpf: 135131351
+    };
+})();
 // 80. Readonly array
+/** Usando o método abaixo não podemos alterar os dados do array por meio da chaves
+ *  é como se criassemos um bloqueio no array.
+ */
+(() => {
+    const nomes = ['Igor', 'Eduardo', 'Caio', 'Marcelo'];
+})();
 // 81. Tuplas
+/** É um tipo de array onde definimos o tipo de quantidade de elementos e os valores da tupla podem
+ * ser alterados com a notação de chaves
+  */
+(() => {
+    const nums = [1, 2, 3, 4];
+    const test = ['Igor', 'Fonseca', 30];
+})();
 // 82. Tuplas com readonly
+(() => {
+    /** Aqui definimos uma tupla read-only, onde o parametro num, que deve ser um array com
+     * dois numeros não pode ser alterado
+     */
+    function showNumbers(num) {
+        console.log(num);
+    }
+    showNumbers([1, 2]);
+})();
 // 83. Conclusão da seção
--- Object type/index.tsx
+++ Object type/index.tsx
@@ -67,10 +67,146 @@
 
 
 // 76. Propriedades readonly
+
+/**A propriedade read-only impede o valor do objeto de ser alterado */
+
+(() => {
+
+    interface CarProps {
+        nome: string,
+        brand: string,
+        readonly wheel: number
+    }
+
+    function showCarsInfos(car: CarProps) {
+        console.log(`${car.nome} is a car made by ${car.brand} and has ${car.wheel} wheels`)
+    }
+
+    const car: CarProps = {
+        nome: 'Palio',
+        brand: 'Fiat',
+        wheel: 4
+    }
+
+    showCarsInfos(car)
+})();
+
 // 77. Index Signature
+
+
+(() => {
+
+    /** Permite criarmos objetos de maneira livre, podem seguindo a sintaxe de que 
+     * a chave deve ser uma string e o valor tbm
+     */
+
+    interface UserProps {
+        [index: string]: string
+    }
+
+    const user: UserProps = {
+        name: 'Caio',
+        age: '30'
+    }
+})();
+
 // 78. Herança de interfaces
+
+/** A idéia por trás da herança é que usando o extends não precisaremos reescrever as propriedades de uma 
+ * interface que queremos usar em outra interface
+ */
+
+(() => {
+
+    interface Products {
+        name: string,
+        price: number
+    }
+
+    // Aqui o extends será usado para passar as props de products para CleaningProducts
+    interface CleaningProducts extends Products {
+        isAvaliable: boolean
+    }
+
+    const sponge: CleaningProducts = {
+        name: 'sponge',
+        price: 2.36,
+        isAvaliable: true
+    }
+})();
+
 // 79. Intersection types
+
+/**Aqui usamos o intersection types para unir duas interfaces em um unico type*/
+
+(() => {
+
+    interface PersonalInfos {
+        name: string
+    }
+
+    interface PrivateInfos {
+        cpf: number
+    }
+
+    type UserInfo = PersonalInfos & PrivateInfos
+
+    const user: UserInfo = {
+        name: 'Igor',
+        cpf: 135131351
+    }
+
+
+})();
+
+
 // 80. Readonly array
+
+/** Usando o método abaixo não podemos alterar os dados do array por meio da chaves
+ *  é como se criassemos um bloqueio no array. 
+ */
+
+(() => {
+
+    const nomes: ReadonlyArray<string> = ['Igor', 'Eduardo', 'Caio', 'Marcelo']
+
+})();
+
+
 // 81. Tuplas
+
+/** É um tipo de array onde definimos o tipo de quantidade de elementos e os valores da tupla podem
+ * ser alterados com a notação de chaves
+  */
+(() => {
+
+    // Tuplas
+    type fourNumbers = [number, number, number, number]
+
+    const nums: fourNumbers = [1, 2, 3, 4]
+
+    type dataMix = [string, string, number]
+
+    const test: dataMix = ['Igor', 'Fonseca', 30]
+
+})();
+
+
 // 82. Tuplas com readonly
+
+(() => {
+
+    /** Aqui definimos uma tupla read-only, onde o parametro num, que deve ser um array com 
+     * dois numeros não pode ser alterado
+     */
+
+    function showNumbers(num: readonly [number, number]) {
+        console.log(num)
+    }
+
+    showNumbers([1, 2])
+
+})()
+
+
 // 83. Conclusão da seção
